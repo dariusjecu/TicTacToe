25b0ec5d9d6be2543febed9e9eb1fa522114ebc0
dariusjecu <dariusjecu@yahoo.com>
2023-02-02 13:37:57 +0000

    New

--- deploy.sh
+++ deploy.sh
@@ -24,4 +24,4 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:dariusjecu/Portfolio.git main:gh-pages
+# git push -f git@github.com:dariusjecu/TicTacToe.git main:gh-pages
